5c8be1927577e64cf5fb4c615624b0fb5f2da2bc
Vantsev <robocop104@mail.ru>
2024-07-05 00:15:14 +0300

    added "Back" button, implemented deleting buttons, added personal information button (not working yet)

--- app/database/requests.py
+++ app/database/requests.py
@@ -3,10 +3,10 @@ from app.database.models import User, Teacher, Student
 from sqlalchemy import select, update, delete
 
 async def set_user(tg_id):
-    async with  async_session() as session:
-        user= await session.scalar(select(User.telegram_id == tg_id))
+    async with async_session() as session:
+        user = await session.scalar(select(User.telegram_id == tg_id))
         if not user:
-            session.add(User(telegram_id = tg_id))
+            session.add(User(telegram_id=tg_id))
             await session.commit()
 
 async def set_user_status(tg_id, status):
--- app/handlers.py
+++ app/handlers.py
@@ -6,8 +6,8 @@ from aiogram.fsm.context import FSMContext
 import app.keyboads as kb
 import app.database.requests as rq
 
-router = Router()
 
+router = Router()
 
 class RegisterForTeachers(StatesGroup):
     initials = State()
@@ -28,31 +28,40 @@ async def cmd_start(message: types.Message, state: FSMContext):
     await state.set_state(RegisterUsers.status)
     await message.answer('Добро пожаловать в бота, который проверяет посещения пар в ГУАПе', reply_markup=kb.main)
 
-
+    # сделать текст показывающий вохможность выбора, а не только приветствие
 
 @router.message(Command('info'))
 async def cmd_info(message: types.Message):
     await message.answer('Im Helping')
 
-
 @router.message(RegisterUsers.status)
 async def register_user(message: types.Message, state: FSMContext):
     if message.text == 'Преподаватель':
         await message.reply('Отлично, теперь необходима пройти регистрацию и подтвердить, что вы преподаватель')
+        await message.answer('Введите ваше имя, фамилию и отчество', reply_markup=kb.back)
         await state.set_state(RegisterForTeachers.initials)
         await rq.set_user_status(message.from_user.id, message.text)
-        await message.answer('Введите ваше имя, фамилию и отчество')
+
     elif message.text == 'Студент':
         await message.reply('Отлично, теперь необходима пройти регистрацию')
+        await message.answer('Введите ваше имя, фамилию и отчество', reply_markup=kb.back)
         await state.set_state(RegisterForStudents.initials)
         await rq.set_user_status(message.from_user.id, message.text)
-        await message.answer('Введите ваше имя, фамилию и отчество')
+
+    else:
+        await message.reply('Пожалуйста, выберите правильный статус: Преподаватель или Студент', reply_markup=kb.main)
+
+
 
 @router.message(RegisterForTeachers.initials)
 async def register_name_for_teacher(message: types.Message, state: FSMContext):
-    await state.update_data(initials=message.text)
-    await state.set_state(RegisterForTeachers.verification_code)
-    await message.answer('Введите код для подтверждения статуса преподавателя')
+    if message.text == 'Назад':
+        await state.set_state(RegisterUsers.status)
+        await message.answer('Вы вернулись к выбору статуса. Пожалуйста, выберите вашу роль:', reply_markup=kb.main)
+    else:
+        await state.update_data(initials=message.text)
+        await state.set_state(RegisterForTeachers.verification_code)
+        await message.answer('Введите код для подтверждения статуса преподавателя', reply_markup=kb.back)
 
 
 @router.message(RegisterForTeachers.verification_code)
@@ -60,15 +69,20 @@ async def register_verification_code(message: types.Message, state: FSMContext):
     await state.update_data(verification_code=message.text)
     data = await state.get_data()
     await message.answer(
-        f'Вы успешно зарегистрированы как преподаватель. \n Ваше ФИО: {data["initials"]} \n Код подтверждения: {data["verification_code"]}')
+        f'Вы успешно зарегистрированы как преподаватель. \n Ваше ФИО: {data["initials"]} \n '
+        f'Код подтверждения: {data["verification_code"]}', reply_markup=kb.info_about_me)
     await state.clear()
 
 
 @router.message(RegisterForStudents.initials)
 async def register_name_for_student(message: types.Message, state: FSMContext):
-    await state.update_data(initials=message.text)
-    await state.set_state(RegisterForStudents.group)
-    await message.answer('Введите вашу учебную группу')
+    if message.text == 'Назад':
+        await state.set_state(RegisterUsers.status)
+        await message.answer('Вы вернулись к выбору статуса. Пожалуйста, выберите вашу роль:', reply_markup=kb.main)
+    else:
+        await state.update_data(initials=message.text)
+        await state.set_state(RegisterForStudents.group)
+        await message.answer('Введите вашу учебную группу')
 
 
 @router.message(RegisterForStudents.group)
@@ -76,5 +90,7 @@ async def register_group(message: types.Message, state: FSMContext):
     await state.update_data(group=message.text)
     data = await state.get_data()
     await message.answer(
-        f'Вы успешно зарегистрированы как студент. \n Ваше ФИО: {data["initials"]} \n Ваша учебная группа: {data["group"]}')
+        f'Вы успешно зарегистрированы как студент. \n Ваше ФИО: {data["initials"]} \n '
+        f'Ваша учебная группа: {data["group"]}', reply_markup=kb.info_about_me)
     await state.clear()
+
