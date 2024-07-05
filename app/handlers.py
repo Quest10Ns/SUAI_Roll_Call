56bc636fc3d3feed4b49e915367eb7a8db136975
Quest10Ns <daniillvanov423734@gmail.com>
2024-07-05 21:10:11 +0300

    now with master changes

--- app/handlers.py
+++ app/handlers.py
@@ -11,6 +11,7 @@ router = Router()
 
 class RegisterForTeachers(StatesGroup):
     initials = State()
+    departmend = State()
     verification_code = State()
 
 
@@ -47,10 +48,17 @@ async def register_user(message: types.Message, state: FSMContext):
         await state.set_state(RegisterForStudents.initials)
         await rq.set_user_status(message.from_user.id, message.text)
         await message.answer('Введите ваше имя, фамилию и отчество')
-
 @router.message(RegisterForTeachers.initials)
 async def register_name_for_teacher(message: types.Message, state: FSMContext):
     await state.update_data(initials=message.text)
+    await rq.set_student_initials_for_teachers(message.from_user.id, message.text)
+    await state.set_state(RegisterForTeachers.departmend)
+    await message.answer('Введите вашу кафедру')
+
+@router.message(RegisterForTeachers.departmend)
+async def register_departmend_for_teachers(message: types.Message, state: FSMContext):
+    await state.update_data(departmend=message.text)
+    await rq.set_departmend_for_teachers(message.from_user.id, message.text)
     await state.set_state(RegisterForTeachers.verification_code)
     await message.answer('Введите код для подтверждения статуса преподавателя')
 
@@ -60,21 +68,26 @@ async def register_verification_code(message: types.Message, state: FSMContext):
     await state.update_data(verification_code=message.text)
     data = await state.get_data()
     await message.answer(
-        f'Вы успешно зарегистрированы как преподаватель. \n Ваше ФИО: {data["initials"]} \n Код подтверждения: {data["verification_code"]}')
+        f'Вы успешно зарегистрированы как преподаватель. \n Ваше ФИО: {data["initials"]} \n Кафедра: {data["departmend"]} \n Код подтверждения: {data["verification_code"]}')
     await state.clear()
+
 
 
 @router.message(RegisterForStudents.initials)
 async def register_name_for_student(message: types.Message, state: FSMContext):
     await state.update_data(initials=message.text)
+    await rq.set_student_initials_for_students(message.from_user.id, message.text)
     await state.set_state(RegisterForStudents.group)
     await message.answer('Введите вашу учебную группу')
+
 
 
 @router.message(RegisterForStudents.group)
 async def register_group(message: types.Message, state: FSMContext):
     await state.update_data(group=message.text)
+    await rq.set_group_for_student(message.from_user.id, message.text)
     data = await state.get_data()
     await message.answer(
         f'Вы успешно зарегистрированы как студент. \n Ваше ФИО: {data["initials"]} \n Ваша учебная группа: {data["group"]}')
     await state.clear()
+
